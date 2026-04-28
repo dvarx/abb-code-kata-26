a150b3235e76126ea1ef9a531e77a228df9ca2d1
dvarx <dvarx@outlook.com>
2026-04-28 09:45:18 +0000

    early return for sulfuras

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -8,6 +8,11 @@ GildedRose::GildedRose(vector<Item> &items) : items(items)
 
 void GildedRose::updateQualityItem(Item &item) const
 {
+    if (item.name == sulfuras)
+    {
+        return;
+    }
+
     if (item.name == aged_brie || item.name == backstage_pass)
     {
         if (item.quality < 50)
@@ -38,17 +43,12 @@ void GildedRose::updateQualityItem(Item &item) const
     {
         if (item.quality > 0)
         {
-            if (item.name != sulfuras)
-            {
-                item.quality = item.quality - 1;
-            }
+
+            item.quality = item.quality - 1;
         }
     }
 
-    if (item.name != sulfuras)
-    {
-        item.sellIn = item.sellIn - 1;
-    }
+    item.sellIn = item.sellIn - 1;
 
     if (item.sellIn < 0)
     {
@@ -69,10 +69,8 @@ void GildedRose::updateQualityItem(Item &item) const
             {
                 if (item.quality > 0)
                 {
-                    if (item.name != sulfuras)
-                    {
-                        item.quality = item.quality - 1;
-                    }
+
+                    item.quality = item.quality - 1;
                 }
             }
         }
