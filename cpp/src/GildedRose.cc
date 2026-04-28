f64e01129d9a88f1d5494de97c9cfe51019324d1
dvarx <dvarx@outlook.com>
2026-04-28 09:41:48 +0000

    refactored logic

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -8,17 +8,7 @@ GildedRose::GildedRose(vector<Item> &items) : items(items)
 
 void GildedRose::updateQualityItem(Item &item) const
 {
-    if (item.name != aged_brie && item.name != backstage_pass)
-    {
-        if (item.quality > 0)
-        {
-            if (item.name != sulfuras)
-            {
-                item.quality = item.quality - 1;
-            }
-        }
-    }
-    else
+    if (item.name == aged_brie || item.name == backstage_pass)
     {
         if (item.quality < 50)
         {
@@ -44,6 +34,16 @@ void GildedRose::updateQualityItem(Item &item) const
             }
         }
     }
+    else
+    {
+        if (item.quality > 0)
+        {
+            if (item.name != sulfuras)
+            {
+                item.quality = item.quality - 1;
+            }
+        }
+    }
 
     if (item.name != sulfuras)
     {
@@ -52,9 +52,20 @@ void GildedRose::updateQualityItem(Item &item) const
 
     if (item.sellIn < 0)
     {
-        if (item.name != aged_brie)
+        if (item.name == aged_brie)
         {
-            if (item.name != backstage_pass)
+            if (item.quality < 50)
+            {
+                item.quality = item.quality + 1;
+            }
+        }
+        else
+        {
+            if (item.name == backstage_pass)
+            {
+                item.quality = item.quality - item.quality;
+            }
+            else
             {
                 if (item.quality > 0)
                 {
@@ -63,17 +74,6 @@ void GildedRose::updateQualityItem(Item &item) const
                         item.quality = item.quality - 1;
                     }
                 }
-            }
-            else
-            {
-                item.quality = item.quality - item.quality;
-            }
-        }
-        else
-        {
-            if (item.quality < 50)
-            {
-                item.quality = item.quality + 1;
             }
         }
     }
